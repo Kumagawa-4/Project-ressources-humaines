4b4bb547e2f16fc9b86cad6dc8acc11ec1da864f
ajnOZDd <allantohaina2@mail.com>
2023-10-01 18:25:39 +0100

    insertion formulaire

--- js/PdfTransformation.js
+++ js/PdfTransformation.js
@@ -1,18 +1,30 @@
 import jsPDF from 'jspdf';
+    
+    var table = new Tabulator("#existing-table", {
+        layout: "fitColumns",
+        selectable: true,
+        pagination: "local",
+        paginationSize: 10,
+        // Add any other Tabulator options you need
+    })
 
-class PdfTransformation {
-    generatePDF() {
-        // mise en place de document 
-        // var data = [
-        //   ["Name", "Age", "City"],
-        //   ["John Doe", "28", "New York"],
-        //   ["Jane Smith", "32", "San Francisco"],
-        //   ["Bob Johnson", "45", "Chicago"]
-        // ];
-  
-        var doc = new jsPDF();
-        doc.table(10, 10, data);
-        var nomDossier = document.querySelector(".nomProjet").value
-        doc.save(nomDossier);
+    function generatePDF() {
+        table.download("pdf", "data.pdf", {
+            orientation:"portrait", 
+            title:"Dynamics Quotation Report", //add title to report
+            jsPDF:{
+                unit:"in", //set units to inches
+            },
+            autoTable:{ //advanced table styling
+                styles: {
+                    fillColor: [100, 255, 255]
+                },
+                columnStyles: {
+                    id: {fillColor: 255}
+                },
+                margin: {top: 60},
+            },
+            documentProcessing:function(doc){
+            }
+        });
     }
-}
